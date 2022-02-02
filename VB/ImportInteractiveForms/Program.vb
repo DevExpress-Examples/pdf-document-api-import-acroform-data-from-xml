cb0b50ec65e04e86d1169a23e605ceeea33f6b77
DevExpressExampleBot <fakeEmail>
2022-02-02 10:20:57 +0300

    Source auto update [skip ci]

--- VB/ImportInteractiveForms/Program.vb
+++ VB/ImportInteractiveForms/Program.vb
@@ -1,17 +1,15 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 
 Namespace ImportInteractiveForms
+
     Friend Class Program
-        Shared Sub Main(ByVal args() As String)
 
-            Using processor As New PdfDocumentProcessor()
-
+        Shared Sub Main(ByVal args As String())
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Load a PDF document with AcroForm data.
                 processor.LoadDocument("..\..\EmptyForm.pdf")
-
                 ' Import AcroForm data from an XML file.
                 processor.Import("..\..\InteractiveForm.xml")
-
                 ' Save the imported document.
                 processor.SaveDocument("..\..\InteractiveForm.pdf")
             End Using
